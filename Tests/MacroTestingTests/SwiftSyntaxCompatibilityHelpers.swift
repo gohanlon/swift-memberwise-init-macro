e4a3090b4fc1a971d60f4f76b18fe2eda1fe37f3
omochimetaru <omochi.metaru@gmail.com>
2025-06-14 12:48:07 +0900

    support SwiftSyntax601

---
+++ Tests/MacroTestingTests/SwiftSyntaxCompatibilityHelpers.swift
@@ -0,0 +1,11 @@
+import SwiftSyntax
+
+extension GenericArgumentSyntax {
+    var argumentCompat600: TypeSyntax? {
+#if canImport(SwiftSyntax601)
+        argument.as(TypeSyntax.self)
+#else
+        argument
+#endif
+    }
+}
