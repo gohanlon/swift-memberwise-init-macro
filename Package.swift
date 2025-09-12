d3bb67e008879ac956a99e0ccdd3c1e10fea8991
omochimetaru <omochi.metaru@gmail.com>
2025-09-12 13:20:07 +0900

    add explicit dependencies

--- Package.swift
+++ Package.swift
@@ -37,7 +37,13 @@ let package = Package(
     .macro(
       name: "MemberwiseInitMacros",
       dependencies: [
+        .product(name: "SwiftBasicFormat", package: "swift-syntax"),
         .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+        .product(name: "SwiftDiagnostics", package: "swift-syntax"),
+        .product(name: "SwiftParser", package: "swift-syntax"),
+        .product(name: "SwiftParserDiagnostics", package: "swift-syntax"),
+        .product(name: "SwiftSyntax", package: "swift-syntax"),
+        .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
         .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
       ]
     ),
